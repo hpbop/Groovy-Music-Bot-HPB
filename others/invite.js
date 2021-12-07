b6aec4a38674664b2f757c77e202284f8776cf99
hpbop <95695903+hpbop@users.noreply.github.com>
2021-12-07 19:39:42 +0530

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -8,11 +8,11 @@ module.exports = {
 
     let inviteEmbed = new MessageEmbed()
       .setTitle("Add us to your server!")
-      .setDescription("Love using Bluepixel bot? Great, Thank you! Consider adding it to your serverso we can get verify soon")
+      .setDescription("Love using HPB's bot? Great, Thank you! Consider adding it to your serverso we can get verify soon")
       .setColor("#F0EAD6")
-      .setAuthor('Harmony','https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif')
+      .setAuthor('HPB','https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif')
       .setThumbnail(message.guild.iconURL())
-      .addField(`Use the following link to add Harmony to your discord server`, 'https://discordapp.com/oauth2/authorize?&client_id=834090978721398824&scope=bot', true)
+      .addField(`Use the following link to add Harmony to your discord server`, 'https://discord.com/api/oauth2/authorize?client_id=909316390417203220&permissions=137476037440&scope=bot', true)
 
     inviteEmbed.setTimestamp();
 
